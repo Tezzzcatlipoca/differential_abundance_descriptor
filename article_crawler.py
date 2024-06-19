ffa93a29c22505487120e681aeaaf30538adcb33
Tezzzcatlipoca <robertoe.francoa@gmail.com>
2024-06-19 23:43:46 +0200

    Ensure paper titles mention condition

--- article_crawler.py
+++ article_crawler.py
@@ -5,6 +5,7 @@ import time
 
 
 def get_nature_links(keyword):
+    keyword = "microbiome and " + keyword
     search_url = f"https://www.nature.com/search?q={keyword.replace(' ', '+')}&article_type=research&order=relevance"
     response = requests.get(search_url)
     time.sleep(3)
@@ -13,13 +14,14 @@ def get_nature_links(keyword):
     for a in soup.find_all('a', href=True):
         title = a.get_text().strip()
         url = a['href']
-        if url.startswith('/articles/'):
+        if url.startswith('/articles/') and keyword.lower() in title.lower():
             full_url = f"https://www.nature.com{url}"
             links.append([title, full_url])
     return links
 
 
 def get_biomedcentral_links(keyword):
+    keyword = "microbiome and " + keyword
     search_url = f"https://www.biomedcentral.com/search?query={keyword.replace(' ', '+')}"
     response = requests.get(search_url)
     time.sleep(3)
@@ -28,13 +30,14 @@ def get_biomedcentral_links(keyword):
     for a in soup.find_all('a', href=True):
         title = a.get_text().strip()
         url = a['href']
-        if ('/articles/' in url) and ('/supplements/' not in url) and ('Full Text' not in title):
+        if ('/articles/' in url) and ('/supplements/' not in url) and ('Full Text' not in title) and keyword.lower() in title.lower():
             full_url = f"https:{url}"
             links.append([title, full_url])
     return links
 
 
 def get_microbiologyresearch_links(keyword):
+    keyword = "microbiome and " + keyword
     search_url = f"https://www.microbiologyresearch.org/search?value1={keyword.replace(' ', '+')}&option1=fulltext"
     response = requests.get(search_url)
     time.sleep(3)
@@ -43,13 +46,14 @@ def get_microbiologyresearch_links(keyword):
     for a in soup.find_all('a', href=True):
         title = a.get_text().strip()
         url = a['href']
-        if ('/content/journal/' in url) and ('read more' not in title.lower()) and (len(title)>15):
+        if ('/content/journal/' in url) and ('read more' not in title.lower()) and (len(title)>15) and keyword.lower() in title.lower():
             full_url = f"https://www.microbiologyresearch.org{url}"
             links.append([title, full_url])
     return links
 
 
 def get_mdpi_links(keyword):
+    keyword = "microbiome and " + keyword
     search_url = f"https://www.mdpi.com/search?q={keyword.replace(' ', '+')}"
     response = requests.get(search_url)
     time.sleep(3)
@@ -60,13 +64,14 @@ def get_mdpi_links(keyword):
         if a:
             title = a.get_text().strip()
             url = a['href']
-            if not url.startswith('http'):
+            if not url.startswith('http') and keyword.lower() in title.lower():
                 url = f"https://www.mdpi.com{url}"
             links.append([title, url])
     return links
 
 
 def get_cambridge_links(keyword):
+    keyword = "microbiome and " + keyword
     search_url = f"https://www.cambridge.org/core/journals/epidemiology-and-infection/listing?q={keyword.replace(' ', '+')}"
     response = requests.get(search_url)
     time.sleep(3)
@@ -75,18 +80,18 @@ def get_cambridge_links(keyword):
     for a in soup.find_all('a', href=True):
         title = a.get_text().strip()
         url = a['href']
-        if ('/core/journals/' in url) and ('/article/' in url) and (len(title)>15):
+        if ('/core/journals/' in url) and ('/article/' in url) and (len(title)>15) and keyword.lower() in title.lower():
             full_url = f"https://www.cambridge.org{url}"
             links.append([title, full_url])
     return links
 
 
-def get_mixed_links(keyword):
+def get_mixed_links(keyword, randomize=False):
     links = get_nature_links(keyword)
     links.extend(get_microbiologyresearch_links(keyword))
     links.extend(get_mdpi_links(keyword))
     links.extend(get_biomedcentral_links(keyword))
     links.extend(get_cambridge_links(keyword))
-    random.shuffle(links)
+    if randomize:
+        random.shuffle(links)
     return links
-
